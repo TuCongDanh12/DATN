b718426f09f9d5875ad04456fcd8cd88271ff0b4
TuCongDanh12 <tucondanh9a1@gmail.com>
2024-02-24 10:54:03 +0700

    Config tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FormPhieunhap from "./component/Form/Muahang/phieunhap";
+function App() {
+  return (
+
+      <div className="App">
+        <FormPhieunhap/>
+      </div>
+
+  );
+}
+
+export default App;
